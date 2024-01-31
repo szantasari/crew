23c3231ba6183c8f61132bd83762d86dabbc4046
szantasari <mehmed.kosongtujuh@gmail.com>
2024-01-31 13:25:29 +0700

    lagi

--- server.js
+++ server.js
@@ -1,46 +1,35 @@
 const express = require('express');
-const cors = require('cors');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Konfigurasi CORS
-app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Konfigurasi Google Spreadsheet
-const doc = new GoogleSpreadsheet('SPREADSHEET_ID');
-const sheetName = 'member';
-const creds = require('./google-credentials.json');
+const doc = new GoogleSpreadsheet('10m5KoW7SDHET7QZYbvIeF12TWJVZjfQfr_EL0gmDVJk');
+const sheetName = 'Sheet1';
+const creds = require('./begitu.json');
 
-// Middleware untuk verifikasi kredensial
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/register.html');
+});
+
+app.post('/register', async (req, res) => {
+  const { name, email } = req.body;
 
   try {
-    // Autentikasi pengguna dengan data di spreadsheet
     await doc.useServiceAccountAuth(creds);
     await doc.loadInfo();
     const sheet = doc.sheetsByTitle[sheetName];
-    const rows = await sheet.getRows();
-
-    let authenticated = false;
-    for (const row of rows) {
-      if (row.Username === username && row.Password === password) {
-        authenticated = true;
-        break;
-      }
-    }
-
-    res.json({ authenticated: authenticated });
+    await sheet.addRow({ Name: name, Email: email });
+    res.json({ message: 'User registered successfully!' });
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
-// Jalankan server
 app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+  console.log(`Server is running on web`);
 });
